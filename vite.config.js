caaa50ad9eee71d8e25061f51240ced2c64ee130
Daniela Romero <clear>
2022-09-20 16:06:48 -0300

    build docs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: "./",
+  base: "/",
+  publicDir: "public",
+  build: {
+    outDir: "docs",
+    assetsDir: "assets"
+  },
 })
