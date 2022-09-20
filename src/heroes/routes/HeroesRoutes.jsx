28e146d0d458a6b37256eea94b7f20cb83066e44
Daniela Romero <clear>
2022-09-20 17:01:47 -0300

    cambio HeroesRoutes

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -17,7 +17,7 @@ export const HeroesRoutes = () => {
                 <Route path="search"  element={ <SearchPage /> } />
                 <Route path="hero/:id"  element={ <HeroPage /> } />
 
-                <Route path="/"  element={ <Navigate to="/marvel" /> } />
+                <Route path="/"  element={ <Navigate to="marvel" /> } />
             </Routes>
         </div>
     </>
