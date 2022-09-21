017da4cc4816246648a21bc6b0043793abb6d684
Daniela Romero <clear>
2022-09-20 23:38:22 -0300

    Cambios en el Navbar

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,6 +1,6 @@
 
 import { useContext } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 import { AuthContext } from '../../auth';
@@ -9,36 +9,33 @@ import { AuthContext } from '../../auth';
 export const NavBar = () => {
 
     const { user, logout } = useContext( AuthContext );
+    const navigate = useNavigate();
 
-    // const navigate = useNavigate();
-
-    // const onLogout = () => {
-    //     logout();
-    //     navigate('/login', {
-    //         replace: true
-    //     });
-    // }
+    const onLogout = () => {
+        logout();
+        navigate('/login', {
+            replace: true
+        });
+    }
 
     return (
         <Navbar collapseOnSelect sticky='top' expand='sm' bg='dark' variant='dark'>
             <Container>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-                <Navbar.Brand href="/">HeroesApp</Navbar.Brand>
+                <Navbar.Brand as={ Link } to="/">HeroesApp</Navbar.Brand>
                 <Navbar.Collapse id='responsive-navbar-nav'>
                     <Nav>
-                        <Nav.Link href='marvel' >Marvel</Nav.Link>
-                        <Nav.Link href='dc' >DC</Nav.Link>
-                        <Nav.Link href='search' >Search</Nav.Link>
+                        <Nav.Link as={ Link } to='/marvel' >Marvel</Nav.Link>
+                        <Nav.Link as={ Link } to='/dc' >DC</Nav.Link>
+                        <Nav.Link as={ Link } to='/search' >Search</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
 
                 <Nav className='d-flex justify-content-end'>
-                    <Navbar.Text>
-                        { user?.name } 
-                        <a href="/login" onClick={ logout } className='p-2' >
-                            <i class="bi bi-box-arrow-right"></i>
-                        </a>
-                    </Navbar.Text>
+                    <Navbar.Text>{ user?.name } </Navbar.Text>
+                    <Nav.Link onClick={ onLogout }> 
+                        <i class="bi bi-box-arrow-right"></i> 
+                    </Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
